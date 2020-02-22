fbf509970bf32620a0936d624cf43cf6ae7237c7
Ryoung27 <RRYoung89@gmail.com>
2020-02-22 15:06:40 -0600

    remove first and last

---
+++ day_seven/twice_as_old.js
@@ -0,0 +1,9 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  // your code here
+  if(dadYearsOld > 2 * sonYearsOld){
+    return(dadYearsOld - (2 * sonYearsOld))
+  }
+  else{
+    return((2 * sonYearsOld) - dadYearsOld) 
+  }
+};
