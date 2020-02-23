9d849eda7c6897f2f5a099c7996b6691e921abb6
Ryoung27 <RRYoung89@gmail.com>
2020-02-23 11:02:09 -0600

    summation

---
+++ day_eight/grasshoper_sum.js
@@ -0,0 +1,7 @@
+var summation = function (num) {
+  let answer = 0
+  for (i = 0; i < num+1; i++) {
+    answer += i
+}
+  return answer
+}
---
+++ day_eight/grasshoper_sum.py
@@ -0,0 +1,5 @@
+def summation(n):
+    a = 0
+    for i in range(n+1):
+        a += i
+    return a
