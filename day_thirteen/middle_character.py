5cc91371c28163a978f1a696636a3a3dc3881df0
Ryoung27 <RRYoung89@gmail.com>
2020-02-28 14:33:09 -0600

    a little faster

--- day_thirteen/middle_character.py
+++ day_thirteen/middle_character.py
@@ -7,3 +7,12 @@ def get_middle(s):
     else:
         value = int(len(s)/2)
         return s[value:value+1:]
+
+def get_middle(s):
+    #your code here
+    value = int(len(s)/2)
+    value_one = value+1
+    if len(s) % 2 == 0:
+        return s[value-1:value_one:]
+    else:
+        return s[value:value_one:]
