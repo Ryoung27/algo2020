26e31f83d69ab32f6647274a5d4050a2cb9d323d
Ryoung27 <RRYoung89@gmail.com>
2020-03-02 18:42:58 -0600

    schafer oop

---
+++ day_sixteen/app.py
@@ -0,0 +1,19 @@
+class Employee:
+	def __init__(self, first, last, pay):
+		self.first = first
+		self.last = last
+		self.pay = pay
+		self.email = first + '.' + last + '@company.com'
+
+	def fullname(self):
+		print(self.first + ' ' + self.last)
+
+emp_1 = Employee("Richie", "Y", 0.00)
+emp_2 = Employee("Test", "U", 10.00)
+
+#print(emp_1)
+#print(emp_2)
+
+print(emp_1.first)
+print(emp_2.first)
+print(emp_1.fullname())
